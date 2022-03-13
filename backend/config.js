f0f90bf09f13546fb4ff73f2e17b5957b7e66a74
Kamil <kamilgarib@gmail.com>
2022-03-13 21:31:01 +0400

    initial

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    CORS_ORIGIN_ALLOW_ALL: false,
+    CORS_ORIGIN_ALLOW_ALL: true,
     CORS_ORIGIN_WHITELIST: [
         "localhost:3000",
         "localhost:5000",
         "garage.novamed.az",
         "api.novamed.az",
+        "http://134.122.84.199:3001",
     ],
     CARS_PAGE_LIMIT: 12,
 };
